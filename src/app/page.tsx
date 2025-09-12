5e0bce5f07a26a69ebd442cc3b71b80298153120
Anusha <anusha@digitalpresencematters.com>
2025-09-12 16:58:53 +0530

    Initial clean commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection'
 import RepairSection from '@/components/RepairSection'
+import WhyChooseUsSection from '@/components/WhyChooseUsSection'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <RepairSection />
+      <WhyChooseUsSection />
     </main>
   )
 }
